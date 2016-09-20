f8419e6fb87f6d0307aa8958b675c7da0a222871
Matthew Farrellee <matt@redhat.com>
2016-09-19 23:10:04 -0400

    james joyce on a socket

---
+++ app.py
@@ -0,0 +1,45 @@
+#!/bin/python
+
+import socket
+import time
+
+import logging
+
+
+def accept(port):
+    sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('0.0.0.0', port))
+    sock.listen(1)
+    return sock.accept()
+
+def main(port, files):
+    logging.debug('main called with port={}, files={}'.format(port, files))
+
+    lines = []
+    for file in files:
+        with open(file) as input:
+            lines += input.readlines()
+
+    while True:
+        logging.info('waiting for connection on port {}'.format(port))
+        conn, addr = accept(port)
+        logging.info('connection from: {}'.format(addr))
+        try:
+            while True:
+                for line in lines:
+                    conn.sendall(line)
+                    time.sleep(0.03)
+        except socket.error as e:
+            logging.debug('received socket error: {}'.format(e))
+        finally:
+            logging.debug('closing connection')
+            conn.close()
+        
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    main(2016, ('james-joyce-ulysses.txt',
+                'james-joyce-a-portrait-of-the-artist-as-a-young-man.txt',
+                'james-joyce-dubliners.txt'))
