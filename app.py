482e944b94cfdaa9e815076e39dbecb884e89775
Tom Chrisstoffels <tom@Toms-MacBook-Pro.local>
2017-03-21 10:04:50 +0100

    James Joyce eruit, verkiezingsprogramma cda erin

--- app.py
+++ app.py
@@ -40,6 +40,4 @@ def main(port, files):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
 
-    main(8080, ('james-joyce-ulysses.txt',
-                'james-joyce-a-portrait-of-the-artist-as-a-young-man.txt',
-                'james-joyce-dubliners.txt'))
+    main(8080, ('cda_2017.txt'))
